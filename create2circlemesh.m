f792f63c1090712310b114654135287082248dd4
Audric GAUTIER <audric.gautier@etu.univ-lyon1.fr>
2025-04-25 18:10:21 +0200

    Add Function to Generate Mesh for 2 circles

---
+++ create2circlemesh.m
@@ -0,0 +1,18 @@
+function [model]=create2circlemesh(x,y,L,R,s_max,order)
+    % x,y : Coordonnées du centre de la construction globale
+    % L : écart entre les deux centres des cercles
+    % R : rayons des cercles
+    % s_max : longueur max des éléments finis 
+    % order : Ordre de l'élément fini utilisé
+
+    C1=[1,x-L/2,y,R]'; % Le premier 1 précise la forme "cercle"
+    C2=[1,x+L/2,y,R]';
+
+    ens_shape=[C1,C2];
+    geom=decsg(ens_shape); % décomposition en faces et arêtes. (EF, Table de Connectivité)
+
+    model=createpde; 
+    geometryFromEdges(model,geom); %création d'une géométrie compréhensible par PDE Toolbox
+
+    generateMesh(model,"hmax",s_max,"GeometricOrder",order)
+end
--- main.m
+++ main.m
@@ -1,7 +1,25 @@
+R=2;
+L=3;
+y=0; 
+x=0;
+
+long_EF_max=0.2;
+ord_EF="Quadratic";
+
+model2c=create2circlemesh(x,y,L,R,long_EF_max,ord_EF)
+
+figure(1)
+pdegplot(model2c,"EdgeLabels","on","FaceLabels","on");
+title("Décomposition Face/Arête de la géométrie deux cercles")
+axis equal
+
+saveas(gcf,"Geom2C.jpg")
+
+figure(2)
+pdemesh(model2c)
+title("Maillage pour la géométrie deux cercles")
+axis equal
+
+saveas(gcf, "Maillage2C.jpg")
 
 
-
-
-hello
-end
-quit
