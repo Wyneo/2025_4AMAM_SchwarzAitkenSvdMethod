484eccc1993aba92d2edd77deb2183950772a3c5
RENOUD HALLEREAU Eowyn <eowyn.renoud-hallereau@etu.univ-lyon1.fr>
2025-05-15 12:28:16 +0200

    Meilleure identification des sous-domaines

--- ApplySubMatrixV1.m
+++ ApplySubMatrixV1.m
@@ -8,24 +8,21 @@ ord_EF="Quadratic";
 
 model=create2circlemodel(x,y,L,R,long_EF_max,ord_EF)
 
-[p,e,t] = meshToPet(model.Mesh); %decomposition PET
-% p : Matrice des points
-% e : Matrice des arêtes 
-% t : Matrice des triangles 
+figure(1)
+pdegplot(model,"EdgeLabels","on","FaceLabels","on");
+title("Décomposition Face/Arête de la géométrie deux cercles")
+axis equal
+saveas(gcf,"Geom2C.jpg")
 
-nodes = p'; %extraction des noeuds
-nn = size(nodes,1); %nombre de noeus
+nn = size(model.Mesh.Nodes,2); %nombre de noeuds
 
-% Identification des sous-domaines (A changer vu comment c'est pas fou)
-inA = vecnorm(nodes - [x+L/2,y], 2, 2) < R; %identifie les différences aux centres et en tire un booléen
-inB = vecnorm(nodes - [x-L/2,y], 2, 2) < R;
-
-idF1 = find(inA & ~inB);
-idF2 = find(inA &  inB);
-idF3 = find(inB & ~inA);
-
-idF12 = union(idF1, idF2); %union des sous domaines
-idF23 = union(idF2, idF3);
+% Identification des sous-domaines 
+idF23=transpose(findNodes(model.Mesh,"region","Face",[2,3]));
+idF23_bord=findNodes(model.Mesh,"region","Edge",[1:5]);
+idF12=transpose(findNodes(model.Mesh,"region","Face",[1,3]));
+idF12_bord=findNodes(model.Mesh,"region","Edge",[5:10]);
+idF23=idF23(~ismember(idF23, idF23_bord));
+idF12=idF12(~ismember(idF12, idF12_bord));
 
 applyBoundaryCondition(model,'dirichlet','Edge',1:model.Geometry.NumEdges,'u',0);
 
@@ -36,7 +33,7 @@ FEM = assembleFEMatrices(model); %all odes matrix
 K = FEM.K; %stiffness matrix (matrice du laplacien)
 F = FEM.F; %vecteur b intégrale
 
-u = zeros(nn,1); %??
+u = zeros(nn,1);
 Niter = 20;
 
 for k = 1:Niter
@@ -48,6 +45,8 @@ for k = 1:Niter
 
     u(I12) = u12(I12); % mise à jour partielle
 
+    %applyBoundaryCondition(model,'dirichlet','Edge',10,'u',u(arc_cercle_gauche),"InternalBC",true);
+
     I23 = idF23;
     u23 = zeros(nn,1);
     K23 = K(I23,I23);
@@ -55,9 +54,11 @@ for k = 1:Niter
     u23(I23) = K23 \ F23;
 
     u(I23) = u23(I23);
+    %applyBoundaryCondition(model,'dirichlet','Edge',3,'u',u(arc_cercle_droite),"InternalBC",true);
 end
 
 pdeplot(model,'XYData',u,'Mesh','on');
 title('Solution FEM avec DDM sur deux cercles intersectés');
 axis equal
 saveas(gcf,"test.jpg")
+
