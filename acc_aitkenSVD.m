4df27b5e68984b3aef508ad7e5863b4b745f3ec3
Audric GAUTIER <audric.gautier@etu.univ-lyon1.fr>
2025-05-30 16:51:02 +0200

    First Version of acc_aitkenSVD (defective)

--- acc_aitkenSVD.m
+++ acc_aitkenSVD.m
@@ -1,12 +1,14 @@
 function res=acc_aitkenSVD(u,epsilon)
-    [U,S,V]=svd(acc_aitkenSVD);
+    [U,S,V]=svd(u);
     sig_vec=diag(S);
     bound_inf=epsilon*S(1,1); % see draft with circles
-    [temp,ind_sig_vec_min]=which.min(abs(sig_vec-bound_inf)); % Detect the nearest of index bound_inf. 
-    [U_rest,S_rest,V_rest]=[U(1:ind_sig_vec_min,1:ind_sig_vec_min),
-                            S(1:ind_sig_vec_min,1:ind_sig_vec_min),
-                            V(1:ind_sig_vec_min,1:ind_sig_vec_min)]; 
-    estim_p=S_rest; 
-    y_inf=(eye(ind_sig_vec_min,ind_sig_vec_min)-estim_p)*(U(:,ind_sig_vec_min+2)-estim_p*U(:,ind_sig_vec_min))
+    [temp,ind_sig_vec_min]=min(abs(sig_vec-bound_inf)); % Detect the nearest of index bound_inf. 
+    U_rest=U(1:ind_sig_vec_min,1:ind_sig_vec_min);
+    S_rest=S(1:ind_sig_vec_min,1:ind_sig_vec_min);
+    V_rest=V(1:ind_sig_vec_min,1:ind_sig_vec_min); 
+    u_err=u(:,2:end)-u(:,1:end-1);
+    estim_p=u_err*U_rest'*pinv(u_err*U_rest'); 
+    disp(size(estim_p))
+    y_inf=(eye(ind_sig_vec_min,ind_sig_vec_min)-estim_p)*(U(:,ind_sig_vec_min+2)-estim_p*U(:,ind_sig_vec_mi+1))
     res=y_inf;
 end
--- main.m
+++ main.m
@@ -6,7 +6,7 @@ L=3;
 y=0; 
 x=0;
 
-long_EF_max=0.2;
+long_EF_max=0.5;
 ord_EF="Quadratic";
 
 % [model2c,mesh2c]=create2circlemesh(x,y,L,R,long_EF_max,ord_EF)
@@ -71,25 +71,70 @@ ord_EF="Quadratic";
 model1=create1circleintermodel(x,y,L,R,long_EF_max,ord_EF)
 model2=create1circleintermodel(x,y,-L,R,long_EF_max,ord_EF)
 
-figure(1)
-pdegplot(model1,"EdgeLabels","on","FaceLabels","on");
-title("Décomposition Face/Arête de la géométrie deux cercles")
-axis equal
-saveas(gcf,"geom.jpg")
-
-cd=findNodes(model1.Mesh,"region","Edge",3);
-
 specifyCoefficients(model1,"m",0,"d",0,"c",1,"a",0,"f",1);
 specifyCoefficients(model2,"m",0,"d",0,"c",1,"a",0,"f",1);
 
-cl_cd=0;
-applyBoundaryCondition(model1,"dirichlet","Edge",[1,2,4,5],"u",0);
-applyBoundaryCondition(model1,"dirichlet","Edge",3,"u",cl_cd);
+% figure(1)
+% pdegplot(model1,"EdgeLabels","on","FaceLabels","on");
+% title("Décomposition Face/Arête de la géométrie deux cercles")
+% axis equal
+% saveas(gcf,"geom.jpg")
 
-results=solvepde(model1);
-u=results.NodalSolution;
-u_cd=u(cd);
+cd=findNodes(model1.Mesh,"region","Edge",3);
+cg=findNodes(model2.Mesh,"region","Edge",5);
 
-figure(2)
-pdeplot(model1.Mesh.Nodes(1,cd),model1.Mesh.Nodes(2,cd),'XYData',u_cd,'Mesh','on');
-saveas(gcf,"Results.jpg")
+% specifyCoefficients(model1,"m",0,"d",0,"c",1,"a",0,"f",1);
+% specifyCoefficients(model2,"m",0,"d",0,"c",1,"a",0,"f",1);
+
+% cl_cd=0;
+% applyBoundaryCondition(model1,"dirichlet","Edge",[1,2,4,5],"u",0);
+% applyBoundaryCondition(model1,"dirichlet","Edge",3,"u",cl_cd);
+
+% results=solvepde(model1);
+% u_cd=interpolateSolution(results,model1.Mesh.Nodes(1,cd),model1.Mesh.Nodes(2,cd));
+
+% Rest_Fun_Diric=@(region,state) evalue(u_cd, region.x, region.y)
+
+[mat_all_iter,cell_all_iter_bord,res_mod_gauche,res_mod_droit]=iter_solve(model1,model2,10); 
+res_bord_droit=acc_aitkenSVD(cell_all_iter_bord{1}); 
+res_bord_gauche=acc_aitkenSVD(cell_all_iter_bord{2});
+
+disp([res_bord_droit,cell_all_iter_bord{1}(:,end)])
+disp([res_bord_gauche,cell_all_iter_bord{2}(:,end)])
+disp(norm(res_bord_droit-cell_all_iter_bord{1}(:,end)))
+disp(norm(res_bord_gauche-cell_all_iter_bord{2}(:,end)))
+
+res_mod_gauche_nodes=res_mod_gauche.NodalSolution; 
+res_mod_gauche_nodes(cd)=res_bord_droit;
+
+% [p,e,t]=meshToPet(model2.Mesh);
+% F2 = pdeInterpolant(p,t,res_mod_gauche_nodes);
+% cl_cd_1=@(region,state) evaluate(F2,region.x,region.y); %condition initiale
+
+% applyBoundaryCondition(model1,"dirichlet","Edge",[1,2,4,5],"u",0);
+% applyBoundaryCondition(model1,"dirichlet","Edge",3,"u",cl_cd_1);
+
+% results1=solvepde(model1);
+
+res_mod_droit_nodes=res_mod_droit.NodalSolution;
+res_mod_droit_nodes(cg)=res_bord_gauche;
+
+% [p,e,t]=meshToPet(model1.Mesh);
+% F1 = pdeInterpolant(p,t,res_bord_gauche);
+% cl_cg_2=@(region,state) evaluate(F1,region.x,region.y); %condition initiale
+
+% applyBoundaryCondition(model2,"dirichlet","Edge",[1,2,3,4],"u",0);
+% applyBoundaryCondition(model2,"dirichlet","Edge",5,"u",cl_cg_2);
+
+% results2=solvepde(model2);
+
+figure(3)
+subplot(1,2,1)
+pdeplot(model1.Mesh,"XYData",res_mod_gauche_nodes) 
+title("Aitken SVD (Partie Gauche)")
+axis equal
+subplot(1,2,2)
+pdeplot(model2.Mesh,"XYData",res_mod_droit_nodes)
+title("Aitken SVD (Partie Droite)")
+axis equal
+saveas(gcf,"Res_After_AccSVD.jpg")
